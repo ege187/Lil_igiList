c98968356d4e3eec200a7c4be55c826ff254aa2e
ege27_ <65175178+ege187@users.noreply.github.com>
2025-12-08 10:57:47 +0100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import DemonListPage from './pages/demonlist.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/demonlist', component: DemonListPage },
 ];
