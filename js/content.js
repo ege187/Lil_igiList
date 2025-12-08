d6ad375390c36300cea4796c856a3fc0b57f8194
ege27_ <65175178+ege187@users.noreply.github.com>
2025-12-08 13:35:37 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -36,6 +36,37 @@ export async function fetchList() {
     }
 }
 
+export async function fetchList2() {
+    const listResult = await fetch(`${dir}/_list2.json`); // zweite Liste
+    try {
+        const list = await listResult.json();
+        return await Promise.all(
+            list.map(async (path, rank) => {
+                const levelResult = await fetch(`${dir}/${path}.json`);
+                try {
+                    const level = await levelResult.json();
+                    return [
+                        {
+                            ...level,
+                            path,
+                            records: level.records.sort(
+                                (a, b) => b.percent - a.percent,
+                            ),
+                        },
+                        null,
+                    ];
+                } catch {
+                    console.error(`Failed to load level #${rank + 1} ${path}.`);
+                    return [null, path];
+                }
+            }),
+        );
+    } catch {
+        console.error(`Failed to load list2.`);
+        return null;
+    }
+}
+
 export async function fetchEditors() {
     try {
         const editorsResults = await fetch(`${dir}/_editors.json`);
